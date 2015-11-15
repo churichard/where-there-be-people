09f0cfd65ce40165025f6c9a3c0a6dc5d0b6ee61
Joshin <JoshuaShin96@gmail.com>
2015-11-15 02:34:32 -0500

    delint and revise sentiment colors

--- app/src/main/java/hackprincetonf2015/wheretherebepeople/MapsActivity.java
+++ app/src/main/java/hackprincetonf2015/wheretherebepeople/MapsActivity.java
@@ -65,8 +65,6 @@ public class MapsActivity extends AppCompatActivity implements
     private MobileServiceTable<Coordinates> mCoordinateTable;
     private MobileServiceTable<Index> mIndexTable;
 
-//    private long thisUser = 12345;
-
     /*
      * Define a request code to send to Google Play services
      * This code is returned in Activity.onActivityResult
@@ -143,7 +141,7 @@ public class MapsActivity extends AppCompatActivity implements
                                         try {
                                             JSONObject jsonObject = new JSONObject(response);
                                             score = jsonObject.optDouble("Score");
-                                            Log.d("Score", score + ""); // TODO use this score for something
+                                            Log.d("Score", score + "");
 
                                         } catch (JSONException e) {
                                             Log.e(TAG, e.toString());
@@ -279,12 +277,7 @@ public class MapsActivity extends AppCompatActivity implements
                 else {
                     float[] hsv = new float[3];
                     double user_score = points.get(i).score;
-                    Color.RGBToHSV((int)(user_score * 255), 0, (int)(255 - (user_score * 255)), hsv);
-//                    if (user_score < 0.5){
-//                        hsv[0] = BitmapDescriptorFactory.HUE_AZURE;
-//                    } else {
-//                        hsv[0] = BitmapDescriptorFactory.HUE_RED;
-//                    }
+                    Color.RGBToHSV((int)(user_score * 255), 0, (int)((1 - user_score) * 255), hsv);
                     options = new MarkerOptions()
                             .position(lastPoint)
                             .icon(BitmapDescriptorFactory.defaultMarker(hsv[0]));
@@ -306,11 +299,6 @@ public class MapsActivity extends AppCompatActivity implements
             float[] hsv = new float[3];
             double user_score = points.get(points.size() - 1).score;
             Color.RGBToHSV((int)(user_score * 255), 0, (int)(255 - (user_score * 255)), hsv);
-//            if (user_score < 0.5){
-//                hsv[0] = BitmapDescriptorFactory.HUE_AZURE;
-//            } else {
-//                hsv[0] = BitmapDescriptorFactory.HUE_RED;
-//            }
             options = new MarkerOptions()
                     .position(lastPoint)
                     .icon(BitmapDescriptorFactory.defaultMarker(hsv[0]));
@@ -381,7 +369,6 @@ public class MapsActivity extends AppCompatActivity implements
             new AsyncTask<Void, Void, MobileServiceList<Coordinates>>() {
                 protected MobileServiceList<Coordinates> doInBackground(Void... no) {
                     MobileServiceList<Coordinates> coordinates = null;
-                    //MobileServiceList<Index> users = null;
                     try {
                         coordinates = mCoordinateTable.
                                 orderBy("userid", QueryOrder.Ascending).
